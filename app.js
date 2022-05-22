8dd78a71106ba8446875c37063ed621e1171e81d
YiHong <english111026@gmail.com>
2022-05-22 22:02:03 +0800

    setting route of shorten&create shorten url

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 const generatePassword = require('./generator')
+const URL = require('./models/url')
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser')
+
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -28,11 +34,22 @@ db.once('open', () => {
 //setting home page route
 
 app.get('/', (req, res) => {
+
   res.render('index')
 })
 
-app.get('/shorten', (req, res) => {
-  res.render('show')
+app.post('/shorten', (req, res) => {
+  const raw_url = req.body.rawURL
+  const url_code = generatePassword()
+  const shorten_url = `http://localhost:3000/${url_code
+    }`
+  const url = new URL({
+    origin_url: raw_url,
+    url_code: url_code
+  })
+  return url.save()
+    .then(() => res.render('show', { shorten_url }))
+    .catch(error => console.log(error))
 })
 
 
